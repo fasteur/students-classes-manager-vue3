bc49a98e5b862044c936f1ff973d95a54205f89f
fasteur <asteur.florian@gmail.com>
2021-04-29 08:35:45 +0200

    feat(SCM-2): Composition Api in Home
    Signed-off-by: fasteur <asteur.florian@gmail.com>

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="home">
-        
         <h1 class="my-4">{{ $t('HOME.TITLE') }}</h1>
-        
+
         <UserFormModalComponent
             :user="student"
             :title="formTitle"
@@ -24,143 +23,144 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component'
-
+import { ref, defineComponent, onMounted, Ref } from 'vue'
 import UserFormModalComponent from '@/components/UserFormModalComponent.vue'
 import UserListComponent from '@/components/UserListComponent.vue'
 import { Student, User } from '../models/index'
 import { UserDto } from '../models/interfaces/dto/user-dto.interface'
 import { studentService } from '../services/student.service'
 
-export interface IHomeData {
-    studentList: Student[]
-    student: Student
-    formTitle: string
-    showModal: boolean
-}
-
-@Options({
+export default defineComponent({
     components: {
         UserListComponent,
         UserFormModalComponent,
     },
-})
-export default class Home extends Vue {
-    // --- DATAS ---
-    public studentList!: Student[]
-    public student!: Student
-    public showModal!: boolean
+    setup() {
+        let studentList: Ref<Student[]> = ref([] as Student[])
+        let student = ref(new Student({}))
+        const formTitle = 'STUDENT_FORM.TITLE'
+        let showModal = ref(false)
 
-    data(): IHomeData {
-        return {
-            studentList: [],
-            student: new Student({}),
-            formTitle: 'STUDENT_FORM.TITLE',
-            showModal: false,
+        onMounted(() => getStudents())
+        
+        function addStudent(data: Student): void {
+            studentService
+                .addStudent(data)
+                .then(() => getStudents())
+                .catch((err) => console.log('err: ', err))
         }
-    }
 
-    // --- LIFE CYCLE HOOKS ---
-    created(): void {
-        this.getStudents()
-    }
-
-    // --- METHODS ---
-
-    // Call services
-
-    private addStudent(data: Student): void {
-        studentService
-            .addStudent(data)
-            .then(() => this.getStudents())
-            .catch((err) => console.log('err: ', err))
-    }
-
-    private getStudents(): void {
-        studentService
-            .getStudents()
-            .then((res) => {
-                if (!res || !res.data) {
-                    this.studentList = []
-                    return
-                }
-                const newStudentList = this.studentListMappingFromDto(res.data)
-                this.studentList = [...newStudentList]
-            })
-            .catch((err) => console.log('err: ', err))
-    }
-
-    public onDeleteStudent(student: Student): void {
-        studentService
-            .deleteStudent(student)
-            .then(() => this.getStudents())
-            .catch((err) => console.log('err: ', err))
-    }
-
-    public editStudent(studentToEdit: Student): void {
-        studentService
-            .editStudent(studentToEdit)
-            .then(() => {
-                this.getStudents()
-                this.student = new Student({})
-            })
-            .catch((err) => console.log('err: ', err))
-    }
-
-    // Dto mapping
-
-    private studentListMappingFromDto(data: UserDto): Student[] {
-        return Object.keys(data).reduce((acc: Student[], curr: string) => {
-            acc.push(
-                new Student({
-                    id: curr,
-                    name: data[curr].name,
-                    firstName: data[curr].firstName,
-                    age: data[curr].age,
+        function getStudents(): void {
+            studentService
+                .getStudents()
+                .then((res) => {
+                    if (!res || !res.data) {
+                        studentList.value = []
+                        return
+                    }
+                    const newStudentList = studentListMappingFromDto(res.data)
+                    console.log('newStudentList: ', newStudentList);
+                    studentList.value = [...newStudentList]
+                    console.log('studentList: ', studentList.value);
                 })
+                .catch((err) => console.log('err: ', err))
+        }
+
+        function onDeleteStudent(student: Student): void {
+            studentService
+                .deleteStudent(student)
+                .then(() => getStudents())
+                .catch((err) => console.log('err: ', err))
+        }
+
+        function editStudent(studentToEdit: Student): void {
+            studentService
+                .editStudent(studentToEdit)
+                .then(() => {
+                    getStudents()
+                    student.value = new Student({})
+                })
+                .catch((err) => console.log('err: ', err))
+        }
+
+        // Dto mapping
+
+        function studentListMappingFromDto(data: UserDto): Student[] {
+            return Object.keys(data).reduce((acc: Student[], curr: string) => {
+                acc.push(
+                    new Student({
+                        id: curr,
+                        name: data[curr].name,
+                        firstName: data[curr].firstName,
+                        age: data[curr].age,
+                    })
+                )
+                return acc
+            }, [])
+        }
+
+        // Student form actions
+
+        function onAddStudent(newStudent: Student): void {
+            formIsValid(newStudent)
+            if (isStudentAlreadyExist(newStudent)) return
+            addStudent(newStudent)
+        }
+
+        function onEditStudent(newStudent: Student): void {
+            formIsValid(newStudent)
+            editStudent(newStudent)
+        }
+
+        /**
+         * @description check if all required form fields are filled
+         */
+        function formIsValid(formValue: User) {
+            if (
+                !formValue ||
+                (formValue &&
+                    !(formValue.name || formValue.age || formValue.firstName))
             )
-            return acc
-        }, [])
-    }
+                return
+        }
 
-    // Student form actions
+        /**
+         * @description set student data & open form modal
+         */
+        function opentEditForm(studentToEdit: Student): void {
+            student.value = new Student({ ...studentToEdit })
+            showModal.value = true
+        }
 
-    public onAddStudent(newStudent: Student): void {
-        this.formIsValid(newStudent)
-        if (this.isStudentAlreadyExist(newStudent)) return
-        this.addStudent(newStudent)
-    }
+        function onResetForm() {
+            student.value = new Student({})
+        }
 
-    public onEditStudent(newStudent: Student): void {
-        this.formIsValid(newStudent)
-        this.editStudent(newStudent)
-    }
+        function isStudentAlreadyExist(newStudent: Student): boolean {
+            const alreadyExist = !!studentList.value.find(
+                (student) =>
+                    student.name == newStudent.name &&
+                    student.age == newStudent.age
+            )
+            return alreadyExist
+        }
 
-    /** 
-     * @description check if all required form fields are filled
-    */
-    private formIsValid(formValue: User) {
-        if (!formValue || (formValue && !(formValue.name || formValue.age || formValue.firstName))) return
-    }
-    
-    /**
-    * @description set student data & open form modal
-    */
-    public opentEditForm(studentToEdit: Student): void {
-        this.student = new Student({ ...studentToEdit })
-        this.showModal = true
-    }
-
-    public onResetForm() {
-        this.student = new Student({})
-    }
-
-    private isStudentAlreadyExist(newStudent: Student): boolean {
-        const alreadyExist = !!this.studentList.find((student) => student.name == newStudent.name && student.age == newStudent.age)
-        return alreadyExist
-    }
-}
+        return {
+            onDeleteStudent,
+            editStudent,
+            onAddStudent,
+            onEditStudent,
+            opentEditForm,
+            onResetForm,
+            student,
+            formTitle,
+            showModal,
+            studentList
+        }
+    },
+})
 </script>
+
 <style lang="scss">
 @import '@/assets/style.scss';
 </style>
